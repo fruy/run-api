0d2b16f93244b7211f291f3b0a95f127236efb5f
ricardoul <ricardoul@hotmail.com>
2016-02-13 18:48:19 -0300

    Cambiado todo

---
+++ build/resources/main/application.groovy
@@ -0,0 +1,9 @@
+// Note that all contents of this file will be merged with application.groovy that wins and overrides application.yml, so remember to remove the same settings in the other (main) file for the same environment, or use only one of these two files
+
+environments {
+/*
+// ok, but due to a Grails issue it's better to move this setting in application.yml,
+// but find a way to pass the value of the desired env var inside it
+ */
+
+}
